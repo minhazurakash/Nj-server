a349c2c78e42240bc93c398a8395192483e7f553
Akash7766 <mraakash01932@gmail.com>
2023-01-05 01:39:14 +0600

    tets

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const { connectToServer } = require('./utils/dbConnect');
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const { connectToServer } = require("./utils/dbConnect");
 const projectsRoutes = require("./routes/v1/projects.route");
 const slidersRoutes = require("./routes/v1/sliders.route");
-const blogsRoutes = require('./routes/v1/blogs.route');
-const instagramRoutes = require('./routes/v1/instagrams.route');
-const userRouter = require('./routes/v1/users.route');
+const blogsRoutes = require("./routes/v1/blogs.route");
+const instagramRoutes = require("./routes/v1/instagrams.route");
+const userRouter = require("./routes/v1/users.route");
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
-app.use(express.json())
-app.use(fileUpload())
+app.use(express.json());
+app.use(fileUpload());
 
 connectToServer((err) => {
-    if (!err) {
-      app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`);
-      });
-    } else {
-      console.log(err);
-    }
-  });
+  if (!err) {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } else {
+    console.log(err);
+  }
+});
 
-  app.get('/', (req, res) => {
-    res.send('Running server')
-  });
-  app.use("/api/v1/project", projectsRoutes);
-  app.use("/api/v1/slider", slidersRoutes);
-  app.use("/api/v1/blog", blogsRoutes);
-  app.use("/api/v1/instagram", instagramRoutes);
-  app.use("/api/v1/user", userRouter);
-
-
+app.get("/", (req, res) => {
+  res.send("Running server");
+});
+app.use("/api/v1/project", projectsRoutes);
+app.use("/api/v1/slider", slidersRoutes);
+app.use("/api/v1/blog", blogsRoutes);
+app.use("/api/v1/instagram", instagramRoutes);
+app.use("/api/v1/user", userRouter);
 
 app.all("*", (req, res) => {
   res.send("No route found.");
